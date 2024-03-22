d371dcfac360bcab3c9a772a1d364f909355cac8
Sharnam <sharnam34@gmail.com>
2024-03-22 16:36:05 +0545

    Dynamic sitemap generation routing

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,22 +1,86 @@
+import portfolioData from "@/app/data/portfolioData";
+
 export default function sitemap() {
-  return [
+  const homepage = "https://twelveletter.co/";
+  // Start with your predefined static sitemap entries
+  const map = [
     {
-      url: "https://twelveletter.co/",
+      url: homepage,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${homepage}about-us`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${homepage}contact-us`,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://twelveletter.co/about-us",
+      url: `${homepage}terms-and-conditions`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
+      changeFrequency: "yearly",
+      priority: 0.1,
     },
     {
-      url: "https://twelveletter.co/contact-us",
+      url: `${homepage}service/digital-marketing`,
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${homepage}service/web-design-and-development`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${homepage}service/graphic-design-expertise`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${homepage}service/content-creation`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${homepage}service/in-house-photoshoot`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
     },
   ];
+
+  // Add dynamic entries from portfolioData
+  portfolioData.forEach((category) => {
+    // Add a URL for the category page
+    map.push({
+      url: `${homepage}${category.categorySlug}`,
+      lastModified: new Date(),
+      changeFrequency: "monthly", // Adjust based on how often these pages are updated
+      priority: 0.8,
+    });
+
+    // Add URLs for portfolio items within the category
+    category.portfolioItems.forEach((item) => {
+      map.push({
+        url: `${homepage}${category.categorySlug}/${item.slug}`,
+        lastModified: new Date(),
+        changeFrequency: "monthly", // Adjust based on how often these pages are updated
+        priority: 0.5,
+      });
+    });
+  });
+
+  // Return the complete sitemap
+  return map;
 }
