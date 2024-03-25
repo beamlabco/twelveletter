1c6fe6a1be6b5d94e6db45ce7bfb47f0592e876b
Sharnam <sharnam34@gmail.com>
2024-03-25 23:05:40 +0545

    Testimonial replacement

--- src/app/components/testimonial/testimonial.jsx
+++ src/app/components/testimonial/testimonial.jsx
@@ -13,12 +13,15 @@ export default function testimonial() {
           tag="h2"
         />
         <p className="mt-5 text-xl font-manrope font-medium tracking-wide max-w-[1200px] leading-relaxed text-black-shade-100 md:mt-7 ">
-          &quot;Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry standard dummy text
-          ever&quot;
+          &quot;Working with the team was transformative for our business. Their
+          communication was exceptional, making us feel connected at every step.
+          The designs they delivered not only wowed us but also perfectly
+          aligned with our brand, receiving rave reviews from our customers.
+          They've redefined excellence for us, and we're excited for what's next
+          in our partnership.&quot;
         </p>
         <p className="mt-5 text-lg font-medium font-manrope text-black-shade-100">
-          Shaya Bajracharya | NY-City Florence
+          Shaya Bajracharya
         </p>
       </div>
     </div>
