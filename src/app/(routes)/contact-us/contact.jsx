7c6b7ec91b3f4811e2c6203fa4dfcb684766e152
Sharnam <sharnam34@gmail.com>
2024-03-25 18:40:04 +0545

    Map fix

--- src/app/(routes)/contact-us/contact.jsx
+++ src/app/(routes)/contact-us/contact.jsx
@@ -242,7 +242,7 @@ export default function Contact(props) {
 
             <iframe
               title="map"
-              src="https://www.google.com/maps/d/u/0/embed?mid=1BQL9p2tnGAJW3qAgNf1kOwkuA-j74eE&ehbc=2E312F&noprof=1"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1249.0922868049493!2d85.31701576360278!3d27.687430091027473!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb197a91083ead%3A0xcb2aec894d81d9d1!2sTwelveletter%20Co.!5e0!3m2!1sen!2snp!4v1711371258374!5m2!1sen!2snp"
               style={{ border: 0 }}
               allowFullScreen={true}
               loading="lazy"
